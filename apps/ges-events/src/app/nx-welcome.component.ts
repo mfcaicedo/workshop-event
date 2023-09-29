c0497fbf0a93e03c188f50a457f9837957d8852a
mfcaicedo <mfcaicedo@unicauca.edu.co>
2023-09-29 10:11:50 -0500

    Microservicios proyecto en Angular

--- apps/ges-events/src/app/nx-welcome.component.ts
+++ apps/ges-events/src/app/nx-welcome.component.ts
@@ -423,7 +423,7 @@ import { CommonModule } from '@angular/common';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome ges-events 👋
+            Bienvenido a la gestión de eventos 👋
           </h1>
         </div>
         <!--  HERO  -->
